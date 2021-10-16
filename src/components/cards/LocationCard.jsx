78a8f5ca83fe4cc2ad3ba759741399400cb102e0
ali yaakoub <ali.yaakoub.2021@gmail.com>
2021-10-16 19:36:39 +0300

    minor .

--- src/components/cards/LocationCard.jsx
+++ src/components/cards/LocationCard.jsx
@@ -18,7 +18,7 @@ const LocationCard = ({item}) => {
             <div className='relative text-xl py-5 rounded-xl shadow-2xl cardBG text-black overflow-hidden'>
                 {/* <h2 className='text-2xl py-2 text-center'>{item.episode}</h2> */}
                 <div className="grid grid-cols-2 items-center">
-                    <strong className='justify-self-end'>Episode's Name :</strong>
+                    <strong className='justify-self-end'>Name :</strong>
                     <h2 className='p-2'> {item.name}</h2>
                     <strong className='justify-self-end'>Dimension : </strong>
                     <p className='p-2'> {item.dimension}</p>
